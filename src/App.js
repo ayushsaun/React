fdbf58b29a4ed67d2762bf773e78e819f521de1f
Ayush <ayushsaun@gmail.com>
2020-05-16 15:59:09 +0530

    React Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './component/menuComponent';
 import './App.css';
+import { DISHES } from "./shared/dishes"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      // so basically we stored all the data of DISHES in dishes a new object
+      dishes : DISHES
+    }
+  }
   render(){
     return (
       <div>
@@ -13,7 +23,9 @@ class App extends Component {
             <NavbarBrand href='/'>RISTORANTE CON FUSION</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        {/* here we made a new array of object dish for this.state which stored all values we got
+         from dishes and we are going to use this dish into our menuComponent for all works */}
+        <Menu dish = {this.state.dishes}/>
       </div>
     );
   }
