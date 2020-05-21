8b4b4a273dfc9c92d4f804aeb39dd3d4d60db77d
Ayush <ayushsaun@gmail.com>
2020-05-21 16:32:54 +0530

    Header and Footer

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './menuComponent';
 import DishDetail from "./DishdetailComponent"
+import Header from './HeaderComponent'
+import Footer from './FooterComponent'
 import { DISHES } from "../shared/dishes"
 
 class Main extends Component {
@@ -26,15 +27,12 @@ onDishSelect(dishId) {
   render(){
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href='/'>RISTORANTE CON FUSION</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         {/* here we made a new array of object dish for this.state which stored all values we got
          from dishes and we are going to use this dish into our menuComponent for all works */}
         <Menu dish = {this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.SelectedDish)[0]} />
+        <Footer />
       </div>
     );
   }
