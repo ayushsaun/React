b65c16ad7f51301cda62ae9c6c364f33131f2d18
Ayush <ayushsaun@gmail.com>
2020-06-21 21:12:11 +0530

    Assignment - 4

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,113 +1,94 @@
-import React, { Component } from 'react'
-import Home from './HomeComponent'
-import Menu from './menuComponent'
-import Contact from './ContactComponent'
-import DishDetail from "./DishdetailComponent"
-import Header from './HeaderComponent'
-import Footer from './FooterComponent'
-import {Switch , Route , Redirect , withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
-import About from './Aboutus'
-import { postComment , fetchDishes , fetchComments , fetchPromos } from '../redux/ActionCreators';
-import { actions } from 'react-redux-form'
-import { TransitionGroup , CSSTransition } from 'react-transition-group'
+import React, { Component } from 'react';
+import Menu from './menuComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './Aboutus';
 
-// here we are getting state values from the state function we made in reducer.js which is accessible from configureStore
-// and we added that in app.js so its accessible everywhere now
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders, postFeedback } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 const mapStateToProps = state => {
-  return {
-    dishes : state.dishes,
-    comments: state.comments,
-    leaders : state.leaders,
-    promotions : state.promotions
-  }   
-}
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    };
+};
 
-// this is for dispatching changes we made to comments
-// so when we click to button of form it will receive parameters in dispatch 
-const mapDispatchToProps = dispatch => ({
-  
-  // addComment is a property which receives these 4 values and then dispatch the function
-  // dispatch recieves addComment as action with all the 4 values
-
-  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
-  fetchDishes : () => {dispatch(fetchDishes())},
-  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
-  fetchComments : () => {dispatch(fetchComments())},
-  fetchPromos : () => {dispatch(fetchPromos())}
-
+const mapDispatchToProps = (dispatch) => ({
+    postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+    fetchPromos: () => { dispatch(fetchPromos()) },
+    fetchComments: () => { dispatch(fetchComments()) },
+    fetchLeaders: () => { dispatch(fetchLeaders()) },
+    postFeedback: (feedback) => dispatch(postFeedback(feedback))
 });
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props)
-  }
-
-  // Called immediately after a component is mounted. Setting state here will trigger re-rendering.
-  componentDidMount() {
-    this.props.fetchDishes();
-    this.props.fetchComments();
-    this.props.fetchPromos();
-  }
-
-  render(){
-
-    // all the Redux state becomes available as props, hence the name mapStateToProps.
-    // So, whatever we were using as this state here will have to be changed to this props,
-    // because the props will come in as properties for the main component,
-    // because we defined it this way, and then connected the main component to my Redux Store.
-    const HomePage = () => {
-      return (
-        <Home dish = {this.props.dishes.dishes.filter((dish) => dish.featured==true)[0]}
-        dishesLoading = {this.props.dishes.isLoading}
-        dishesErrMess = {this.props.dishes.errMess}
-        promotion = {this.props.promotions.promotions.filter((promo) => promo.featured==true)[0]}
-        promosLoading = {this.props.promotions.isLoading}
-        promosErrMess = {this.props.promotions.errMess}
-        leader = {this.props.leaders.filter((leader) => leader.featured==true)[0]}  />
-      )
+    componentDidMount() {
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+        this.props.fetchLeaders();
     }
 
-    const DishWithId = ({match}) => {
-      return(
-        // parseInt will convert the string coming from match.params.dishId into a base 10 integer as mentioned in
-        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        dishesLoading = {this.props.dishes.isLoading}
-        dishesErrMess = {this.props.dishes.errMess}
-        comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        commentsErrMess = {this.props.comments.errMess}
-        postComment={this.props.postComment}  
-      />
-        // the addComment we made in MainComponent.js will be passed to dishDetail as parameter
-        // so we can use it to dispatch the action to store
-      )
-    }
+    render() {
+        const HomePage = () => {
+            return (
+                <Home dish={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.promotions.filter(promo => promo.featured)[0]}
+                    promosLoading={this.props.promotions.isLoading}
+                    promosErrMess={this.props.promotions.errMess}
+                    leader={this.props.leaders.leaders.filter(leader => leader.featured)[0]}
+                    leadersLoading={this.props.leaders.isLoading}
+                    leadersErrMess={this.props.leaders.errMess}
+                />
+            );
+        }
 
-    return (
-      <div>
-        <Header />
-        <TransitionGroup>
-          <CSSTransition key={this.props.location.key} classNames="page" timeout={300} >
-            <Switch>
-              <Route path="/home" component = {HomePage} />
-              <Route path="/aboutus" component={() => <About leaders={this.props.leaders} /> } />
-              <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
-              {/* earlier in menu we made whole card into a link which on clicking will show the dish detail and comments on it
-              so that link was connected using the way given below where we received the link and then passed it to DishWithId */}
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path="/contactus" component = {() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-              <Redirect to='/home' />
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
-        <Footer />
-      </div>
-    );
-  }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail
+                    dish={this.props.dishes.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    comments={this.props.comments.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
+                    commentsErrMess={this.props.comments.errMess}
+                    postComment={this.props.postComment}
+                />
+            );
+        }
+
+        return (
+            <div>
+                <Header />
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path="/home" component={HomePage} />
+                            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
+                            <Route path="/menu/:dishId" component={DishWithId} />
+                            <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
+                            <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
+                            <Redirect to="/home" />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+                <Footer />
+            </div>
+        );
+    }
 }
 
-// for exporting purpose we export our content like this
-// export default (connect(mapStateToProps)(Main));
-// in case we are working with reactrouter we need to enclose this with withRouter as given below
-export default withRouter(connect(mapStateToProps , mapDispatchToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,30 +1,22 @@
-// this is to import everything as action type in this folder
-import * as ActionTypes from './ActionTypes'
+import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
 
-//this is a function that creates action object
-export const addComment =  (comment) => ({
-    //every action object must contain type
+export const addComment = (comment) => ({
     type: ActionTypes.ADD_COMMENT,
-
-    // The payload contains whatever needs 
-    // to be carried in so the data that is sent back by the addComment.
-    //it is a very natural way of specifying whatever data needs to be carried in the action 
-    // object to the reducer function 
     payload: comment
-})
+});
 
-export const postComment = (dishId , rating , author , comment) => (dispatch) => {
-   
+export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     const newComment = {
         dishId: dishId,
         rating: rating,
         author: author,
-        comment: comment
-    }
+        comment: comment,
+    };
+
     newComment.date = new Date().toISOString();
 
-    return fetch(baseUrl + 'comments' , {
+    return fetch(baseUrl + 'comments', {
         method: 'POST',
         body: JSON.stringify(newComment),
         headers: {
@@ -32,61 +24,54 @@ export const postComment = (dishId , rating , author , comment) => (dispatch) =>
         },
         credentials: 'same-origin'
     })
-    
-    .then(response => {
-        if (response.ok) {
-            return response  // this response then become available to next response
-        }
-        else {
-            var error = new Error('Error'+ response.status + ':' + response.statusText);
-            error.response = response;
-            throw error;
-        }
-    },
-    error => {
-        var errmess = new Error(error.message);
-        throw errmess;
-    })
-    .then(response => response.json())
-    .then(response => dispatch(addComment(response)))
-    .catch(error =>  { console.log('post comments', error.message); 
-                    alert('Your comment could not be posted\nError: '+error.message);
-                    }
-        );
-}
-
-// we are going to create this as thunk thats why its going to return a function thats why empty ()
-// and inside that there will be another function 
-export const fetchDishes = () => (dispatch) => {
-
-    dispatch(dishesLoading(true));
-
-    return fetch(baseUrl + 'dishes')
-
         .then(response => {
             if (response.ok) {
-                return response  // this response then become available to next response
-            }
-            else {
-                var error = new Error('Error'+ response.status + ':' + response.statusText);
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
                 error.response = response;
+
                 throw error;
             }
         },
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
+            error => {
+                var errorMessage = new Error(error.errorMessage);
+                throw errorMessage;
+            }
+        )
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('Post comments: ' + error.message);
+            alert('Comments could not be posted:\n' + error.message)
         })
-        .then(response => response.json()) //this will convert it into json
-        // Now once we have done that, then we need to, Take that JSON.
-        // So once the JSON, so this response.json will convert that to response.json and
-        // then it will become available here.
-        // And then we'll call that as the parameter as a dish, dishes parameter.
+};
+
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+
+                throw error;
+            }
+        },
+            error => {
+                var errorMessage = new Error(error.errorMessage);
+                throw errorMessage;
+            }
+        )
+        .then(response => response.json())
         .then(dishes => dispatch(addDishes(dishes)))
         .catch(error => dispatch(dishesFailed(error.message)))
 }
 
-export const dishesLoading = () => ({
+export const dishesLoading = () => (dispatch) => ({
     type: ActionTypes.DISHES_LOADING
 });
 
@@ -100,32 +85,25 @@ export const addDishes = (dishes) => ({
     payload: dishes
 });
 
-
-
-
 export const fetchComments = () => (dispatch) => {
-
     return fetch(baseUrl + 'comments')
-
         .then(response => {
             if (response.ok) {
-                return response  // this response then become available to next response
-            }
-            else {
-                var error = new Error('Error'+ response.status + ':' + response.statusText);
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
                 error.response = response;
+
                 throw error;
             }
         },
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        })
-        .then(response => response.json()) //this will convert it into json
-        // Now once we have done that, then we need to, Take that JSON.
-        // So once the JSON, so this response.json will convert that to response.json and
-        // then it will become available here.
-        // And then we'll call that as the parameter as a dish, dishes parameter.
+            error => {
+                var errorMessage = new Error(error.errorMessage);
+                throw errorMessage;
+            }
+        )
+
+        .then(response => response.json())
         .then(comments => dispatch(addComments(comments)))
         .catch(error => dispatch(commentsFailed(error.message)))
 }
@@ -140,38 +118,32 @@ export const addComments = (comments) => ({
     payload: comments
 });
 
-
-
 export const fetchPromos = () => (dispatch) => {
-
     dispatch(promosLoading(true));
 
     return fetch(baseUrl + 'promotions')
-                
         .then(response => {
             if (response.ok) {
-                return response  // this response then become available to next response
-            }
-            else {
-                var error = new Error('Error'+ response.status + ':' + response.statusText);
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
                 error.response = response;
+
                 throw error;
             }
         },
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        })
-        .then(response => response.json()) //this will convert it into json
-        // Now once we have done that, then we need to, Take that JSON.
-        // So once the JSON, so this response.json will convert that to response.json and
-        // then it will become available here.
-        // And then we'll call that as the parameter as a dish, dishes parameter.
+            error => {
+                var errorMessage = new Error(error.errorMessage);
+                throw errorMessage;
+            }
+        )
+
+        .then(response => response.json())
         .then(promos => dispatch(addPromos(promos)))
         .catch(error => dispatch(promosFailed(error.message)))
 }
 
-export const promosLoading = () => ({
+export const promosLoading = () => (dispatch) => ({
     type: ActionTypes.PROMOS_LOADING
 });
 
@@ -185,4 +157,75 @@ export const addPromos = (promos) => ({
     payload: promos
 });
 
-//we send this to the store 
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading(true));
+
+    return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+
+                throw error;
+            }
+        },
+            error => {
+                var errorMessage = new Error(error.errorMessage);
+                throw errorMessage;
+            }
+        )
+
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)))
+}
+
+export const leadersLoading = () => (dispatch) => ({
+    type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
+});
+
+export const postFeedback = (feedback) => (dispatch) => {
+    const newFeedback = Object.assign({ date: new Date().toISOString() }, feedback);
+    
+    return fetch(baseUrl + 'feedback', {
+        method: 'POST',
+        body: JSON.stringify(newFeedback),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+
+                throw error;
+            }
+        },
+            error => {
+                var errorMessage = new Error(error.errorMessage);
+                throw errorMessage;
+            }
+        )
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('Post feedback: ' + error.message);
+            alert('Feedback could not be posted:\n' + error.message)
+        })
+};
